dd89be08eb3062a4ddf1e70d3566aab9d02cb4c0
152056201 <1767977142@qq.com>
2018-11-27 16:57:12 +0800

    change hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
-public void hello{
+public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello Git");
+	}
+	pubic static void add(){
+		System.out.println("啦啦啦啦啦");
 	}
 }
