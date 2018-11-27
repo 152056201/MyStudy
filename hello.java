061207f815b6fea1d1351369c4fb7498219edb06
152056201 <1767977142@qq.com>
2018-11-27 20:23:46 +0800

    last

--- hello.java
+++ hello.java
@@ -1,9 +1,14 @@
-public class hello{
+public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello Git");
 		add();
+		Hello hello = new Hello();
+		hello.Delete();
 	}
 	pubic static void add(){
 		System.out.println("啦啦啦啦啦");
+	}
+	public void Delete(){
+		System.out.println("我要删除你");
 	}
 }
